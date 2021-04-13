f942eb38f96503a3bc5b48648acfd234b58cfcea
Clayton Novotney <claytonnovotney@gmail.com>
2021-04-13 10:26:31 -0700

    updated the local page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 const App = () => {
-  return (
-    <div>
-      <h1>Clayton Novotney</h1>
-      <h2>Info 474</h2>
-      <h2>April 13th, 2021</h2>
-      <h4>Hello World Demo</h4>
-    </div>
-  );
+    console.log("Hello world in the console.");
+    return (
+        <div>
+        <h1>Clayton Novotney</h1>
+        <h2>Info 474</h2>
+        <h2>April 13th, 2021</h2>
+        <h4>Hello World Demo With React and Javascript</h4>
+        </div>
+    );
 };
 
 export default App;
